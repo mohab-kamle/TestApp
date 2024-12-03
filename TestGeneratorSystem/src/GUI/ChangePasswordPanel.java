286bd58e53d87b88449739d0e4cf47361f2a3f7c
ziad <ziad1124204@outlook.com>
2024-12-03 20:09:46 +0200

    small updates.

---
+++ TestGeneratorSystem/src/GUI/ChangePasswordPanel.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+import EndUser.Admin;
+import EndUser.Student;
+import EndUser.User;
+
+/**
+ *
+ * @author Zeyad
+ */
+public class ChangePasswordPanel {
+
+    public ChangePasswordPanel(User user) {
+        if (user instanceof Admin){
+            Admin admin = (Admin)user ;
+        }
+        else if (user instanceof Student){
+            Student student = (Student)user ;
+        }
+    }
+    
+}
